72659536e56c99a8f930030f4ff6d2eb37e92e78
RickCarr <rick.carr@telus.com>
2022-11-14 19:06:30 -0800

    building assertEqual.js for LHL use of console.assert

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😎 Assertion Passed 😎: " + actual + " === " + expected);
+  } else {
+    console.log("😞 Assertion Failed 😞: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
