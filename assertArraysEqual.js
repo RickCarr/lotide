b9998d25ef5f8ed844a944c4ead57f62ca0088b2
RickCarr <rick.carr@telus.com>
2022-11-16 10:23:26 -0800

    function that validates two arrays with assertion

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arrayA, arrayB) {
+  const reset = "\x1b[0m";
+  const fgRed = "\x1b[31m";
+  const fgGreen = "\x1b[32m";
+  let result = true;  //set default to true
+  if (arrayA.length !== arrayB.length) result = false; //fails if arrays are diff lengths
+  for (let i = 0; i < arrayA.length; i++) { //loops index for aligned length
+    if (arrayA[i] !== arrayB[i]) result = false; //fails if any index of the arrays don't match
+  }
+  result === true ? console.log(`😎 Great Success!! 😎: \n ` + fgGreen, `${arrayA} === ${arrayB}`, reset)
+    : console.tabl(`😞 Failure Detected!! 😞: \n` + fgRed, `${arrayA} === ${arrayB}`, reset);
+};
+//assertions listed below
+const apple = "this is an apple";
+eqArrays([1, 2, 3], [1, 2, 3]); //true
+eqArrays(["banana", 2, apple], ["banana", 2, apple]); //true
+eqArrays(["banana", 2, apple],["banana", 2, "apple"]); //false
+eqArrays(["banana", 2, apple],["banana", 3, "this is an apple"]); //false
