6ca2a71de9938e4e3cb57e8b5b65cce0765450c3
RickCarr <rick.carr@telus.com>
2022-11-16 19:56:42 -0800

    adding Object and array counting cross check

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎 Assertion Passed 😎: ${actual} === ${expected}`);
+  } else {
+    console.log(`😞 Assertion Failed 😞: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+    for (const item of allItems) {
+      if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }  
+  return results;
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kevith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
