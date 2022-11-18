e0f86cc32734c3ed64d0830dcedafa95d4b2565d
RickCarr <rick.carr@telus.com>
2022-11-18 02:32:56 -0800

    updating edited map.js & created eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎 Assertion Passed 😎: \x1b[32m ${actual}  === ${expected} \x1b[0m`);
+  } else {
+    console.log(`😞 Assertion Failed 😞: \x1b[31m ${actual} !== \x1b[32m ${expected} \x1b[0m`);
+  }
+};
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+
+    return false;
+  }
+
+  for (let index = 0; index < arrayA.length; index++) {
+    if (arrayA[index] !== arrayB[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function(object1, object2) {
+  const keyRing1 = Object.keys(object1).sort();
+  const keyRing2 = Object.keys(object2).sort();
+  if (keyRing1.length !== keyRing2.length) {
+    return false;
+  }
+  for (let key of keyRing1) {
+    const key1 = object1[key];
+    const key2 = object2[key];
+    if (Array.isArray(key1) || Array.isArray(key2)) {
+      if (eqArrays(key1, key2) === false) {
+        return false;
+      }
+    } else if (key1 !== key2) {
+      return false;
+    }
+  } 
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // -> true
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
--- map.js
+++ map.js
@@ -1,12 +1,6 @@
-const words = ["hey", "dumb", "tralala", "close", "to", "the", "edit"];
-
-
-const map = function(array, callback) {
-  const results = [];
-  for (let item of array) {
-    results.push(callback(item));
-  }
-  return results;
+const assertArraysEqual = function(arr1, arr2) {
+  const actual = eqArrays(arr1, arr2);
+  actual === true ? console.log(`😎 Arrays Match!! 😎: \n \x1b[32m ${arr1} === ${arr2} \x1b[0m`) : console.log(`😞 Arrays Don't Match!! 😞: \n  \x1b[31m ${arr1} !== \x1b[32m ${arr2} \x1b[0m`);
 };
 
 const eqArrays = function(arrayA, arrayB) {
@@ -22,10 +16,17 @@ const eqArrays = function(arrayA, arrayB) {
   return true;
 };
 
-const assertArraysEqual = function(arr1, arr2) {
-  const actual = eqArrays(arr1, arr2);
-  actual === true ? console.log(`😎 Arrays Match!! 😎: \n \x1b[32m ${arr1} === ${arr2} \x1b[0m`) : console.log(`😞 Arrays Don't Match!! 😞: \n  \x1b[31m ${arr1} !== \x1b[32m ${arr2} \x1b[0m`);
+const words = ["hey", "dumb", "tralala", "close", "to", "the", "edit"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
 };
+
 
 const mapped1 = map(words, word => word + "!");
 const mapped2 = map(words, word => word + "");
@@ -36,4 +37,4 @@ const mapped4 = map(words, word => (words[3]));
 assertArraysEqual(mapped1, ["hey!", "dumb!", "tralala!", "close!", "to!", "the!", "edit!"]);
 assertArraysEqual(mapped2, words);
 assertArraysEqual(mapped3, ["Paranoimia", "Paranoimia", "Paranoimia", "Paranoimia", "Paranoimia", "Paranoimia", "Paranoimia"]);
-assertArraysEqual(mapped4, `${words[4]} ${words[5]} ${words[6]}`); //fails
+assertArraysEqual(mapped4, `${words[4]} ${words[5]} ${words[6]}`); //this should fail for 
