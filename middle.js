314eadbe56c6563a452320032cebaf382ed1b778
RickCarr <rick.carr@telus.com>
2022-11-21 14:51:36 -0800

    refactor to export test to middleTest.js

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    return false;
-  }
-  for (let index = 0; index < arrayA.length; index++) {
-    if (arrayA[index] !== arrayB[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  const actual = eqArrays(arr1, arr2);
-  actual === true ? console.log(`😎 Arrays Match!! 😎: \n \x1b[32m ${arr1} === ${arr2} \x1b[0m`) :
-  console.log(`😞 Arrays Don't Match!! 😞: \n  \x1b[31m ${arr1} !== \x1b[32m ${arr2} \x1b[0m`);
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -26,6 +8,8 @@ const middle = function(array) {
   }
   return array;
 };
+
+module.exports = middle;
 //alt method, with more code.
   //   let center = array.length / 2
   //   if (array.length <= 2) {
@@ -37,7 +21,3 @@ const middle = function(array) {
   //     return array.slice(center, center + 1);
   //   }
   // };
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual((middle([1, 2, 3])), [2]);
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);
