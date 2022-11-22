7dea7332c436dff87f933d5f1b973a6c6d0c9c4e
RickCarr <rick.carr@telus.com>
2022-11-21 18:28:00 -0800

    removed excess code line

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -4,4 +4,3 @@ const cat = `"playss with string"`;
 assertArraysEqual(["Using numbers", 1, 2, 3], ["Using numbers", 1, 2, 3]);
 assertArraysEqual(["Using", " const cat =", cat], ["Using", " const cat =", cat]);
 assertArraysEqual(["Lana", "Billie", "Zella"], ["Lana", "Billie", "Zella"]);
-assertArraysEqual(["1337", "IMAGE", 36 + "VWI"], ["1337", "IMAGE", 36 + "VWI"]);
